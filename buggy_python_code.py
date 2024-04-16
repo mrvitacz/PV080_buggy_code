86fdbd111573bb2c6869f0f5b6969f575b9c9d75
unknown <xjira@ad.fi.muni.cz>
2024-04-16 15:01:42 +0200

    codeql

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -64,4 +64,4 @@ if __name__ == '__main__':
         print("Executed -ls on current folder")
     elif choice == "4":
         password = input("Enter master password: ")
-        authenticate(password)
+        authenticate(password) 
